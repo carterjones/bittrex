5007e30faed27b1a2cc58242d612d9c711ad0cc1
Carter Jones <carter@carterjones.info>
2018-03-11 16:49:34 -0700

    iterate on trade struct

--- trade.go
+++ trade.go
@@ -1,6 +1,8 @@
 package bittrex
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -21,8 +23,29 @@ func (t *Trade) Market() string {
 	return t.BaseCurrency + "-" + t.MarketCurrency
 }
 
+func (t Trade) String() string {
+	mkt := t.Market()
+	typ := t.Type.String()
+	tim := t.Time.String()
+	prc := strconv.FormatFloat(t.Price, 'f', 8, 64)
+	qty := strconv.FormatFloat(t.Quantity, 'f', 8, 64)
+	return fmt.Sprintf("%s: %s | %s | quantity: %s | price: %s",
+		mkt, typ, tim, prc, qty)
+}
+
 // TradeType represents a type of trade: either buy or sell.
 type TradeType int
+
+func (t TradeType) String() string {
+	switch t {
+	case BuyType:
+		return "BUY"
+	case SellType:
+		return "SELL"
+	default:
+		return "<invalid trade type>"
+	}
+}
 
 const (
 	// InvalidTradeType is used as the default for the Type type to indicate a
